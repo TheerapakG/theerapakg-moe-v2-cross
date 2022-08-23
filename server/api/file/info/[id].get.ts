2e3dd89bd15903c2f4bf514638e5c405069ab2c9
TheerapakG <theerapakg@gmail.com>
2022-08-23 12:51:30 +0700

    add auth for info

--- server/api/file/info/[id].get.ts
+++ server/api/file/info/[id].get.ts
@@ -3,21 +3,41 @@ import path from "path";
 import { useRedis } from "~/utils/useRedis";
 
 export default defineEventHandler(async (event) => {
-  try {
-    const dir = await useRedis().get(`file:${event.context.params.id}`);
+  const query = useQuery(event);
 
-    if (dir) {
-      return {
-        status: 0,
-        value: {
-          name: path.basename(dir),
-          size: (await fs.promises.stat(dir)).size,
-          url: `/api/file/download/${event.context.params.id}`,
-        },
-      };
+  const user = await useRedis().get(`session:${query.session ?? "default"}`);
+
+  if (
+    user &&
+    (
+      await useRedis()
+        .multi()
+        .sismember(`${user}:perms`, "perms:file:view")
+        .sismember(`file:${event.context.params.id}:perms:view`, user)
+        .exec()
+    )[1].some((i) => i > 0)
+  ) {
+    try {
+      const dir = await useRedis().get(`file:${event.context.params.id}`);
+
+      if (dir) {
+        return {
+          status: 0,
+          value: {
+            name: path.basename(dir),
+            size: (await fs.promises.stat(dir)).size,
+            url: `/api/file/download/${event.context.params.id}`,
+          },
+        };
+      }
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
+  } else {
+    return {
+      status: -8,
+      error: "no permission",
+    };
   }
 
   return {
