bcce827884c25c482d4c71bf9dea6843027d1a1f
TheerapakG <theerapakg@gmail.com>
2022-12-25 20:30:00 +0700

    reuse setup workflow, change node ver

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import vuejsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   plugins: [vue(), vuejsx()],
   test: {
     environment: "jsdom",
