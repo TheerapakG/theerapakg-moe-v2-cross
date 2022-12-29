5d95241ad0209b9c426048d34894c6cecbeabafb
TheerapakG <theerapakg@gmail.com>
2022-12-29 23:31:28 +0700

    coerce vite plugin type for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import vuejsx from "@vitejs/plugin-vue-jsx";
+import { Plugin } from "vitest/node_modules/vite";
 
 export default defineConfig({
-  plugins: [vue(), vuejsx()],
+  plugins: [vue(), vuejsx()] as Plugin[],
   test: {
     environment: "jsdom",
     deps: {
